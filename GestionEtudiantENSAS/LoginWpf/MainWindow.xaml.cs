64c63d1362ea4a6fbd803f83806e20a5160759c1
Douiab <asmaa.douiab@gmail.com>
2018-03-14 01:33:09 +0100

    -a

---
+++ GestionEtudiantENSAS/LoginWpf/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginWpf
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int i;
+        public MainWindow()
+        {
+            InitializeComponent();
+            i = 0;
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+			this.Close();
+        }
+
+        private void ButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (UserName.Text.ToString().Equals("admin") && Pwd.Password.ToString().Equals("12345"))
+            {
+                WindowPrincipale windows = new WindowPrincipale();
+                windows.Show();
+                this.Close();
+            }
+            else
+            {
+                i++;
+                if (i < 3)
+                {
+                    MsgError.Text = "Login or Password is incorrect";
+                }
+                else if (i >= 3 && i < 5)
+                {
+                   
+                }
+                else
+                {
+                    MsgError.Text = "Vous avez fait plus de 4 essaies l'application va s'arreter WARNING !!!";
+                    Application.Current.Shutdown();
+                }
+            }
+
+        }
+    }
+}
---
+++ GestionEtudiantENSAS/LoginWpf/WindowPrincipale.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoginWpf
+{
+    /// <summary>
+    /// Logique d'interaction pour WindowPrincipale.xaml
+    /// </summary>
+    public partial class WindowPrincipale : Window
+    {
+        public WindowPrincipale()
+        {
+            InitializeComponent();
+        }
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonClose2.Visibility = Visibility.Collapsed;
+            ButtonClose.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonClose.Visibility = Visibility.Collapsed;
+            ButtonClose2.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+        }
+    }
+}
